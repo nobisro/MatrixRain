c98e339f5a6b7f6a6de90ac45e017bac4b2efd83
Rohan <nobisro@gmail.com>
2018-06-23 12:32:47 +0800

    streams entire length of window, first symbol occasionally light green

--- sketch.js
+++ sketch.js
@@ -1,35 +1,48 @@
-let symbolSize = 60;
-let stream;
+let symbolSize = 26;
+let streamLength = window.innerWidth / symbolSize;
+let streams = [];
 
 function setup() {
     createCanvas(
         window.innerWidth,
         window.innerHeight
     );
+    background(0, 150 );
+    let x = 0;
+  
+    for (let i = 0; i < streamLength; i++) {
+        let stream = new Stream();
+        stream.generateSymbols(x, random(-1000, 0));
+        streams.push(stream);
+        x += symbolSize;
+    }
     textSize(symbolSize);
-    stream = new Stream();
-    //generate symbols in setup
-    stream.generateSymbols();
+
 }
 
 function draw() {
-    background(0)
-    //render stream (of symbols) in draw
-    stream.render();
+    background(0, 120);
+    streams.forEach(function(stream) {
+        stream.render();
+    });
+    
 }
+
 // Symbol class
-function Symbol(x, y, speed) {
+function Symbol(x, y, speed, first) {
     this.x = x;
     this.y = y;
     this.value;
     this.speed = speed;
     this.switchInterval = round(random(6, 30));
+    this.first = first;
 
     this.setToRandomSymbol = function() {
         // set switch interval
         if (frameCount % this.switchInterval == 0) {
             this.value = String.fromCharCode(
-                0x3400 + round(random(0, 6592)) // Chinese
+                0x30A0 + round(random(-96, 96))
+                // 0x3400 + round(random(0, 6592)) 
             );
         }
     }
@@ -59,24 +72,27 @@ function Stream() {
     //Each stream should know how many symobls it has (5 - 30)
     this.totalSymbols = round(random(5, 20));
     //Each stream should know how fast it's traveling at (5 - 20)
-    this.speed = random(5, 20);
+    this.speed = random(5, 15);
     //Responsibility of each stream to create all of its symbols
     //use a loop
-    this.generateSymbols = function () {
-        let y = 0; //Start at top of page
-        let x = width / 2 // Middle of canvas
-
+    this.generateSymbols = function (x, y) {
+        let first = round(random(0, 4)) == 1; // true = 1 == 1
         for (let i = 0; i < this.totalSymbols; i++) {
-            symbol = new Symbol(x, y, this.speed);
+            symbol = new Symbol(x, y, this.speed, first);
             symbol.setToRandomSymbol();
             this.symbols.push(symbol);
             y -= symbolSize; //symbolSize is in pixels, y is decremented
+            first = false;
         }
     }
 
     this.render = function () {
         this.symbols.forEach(function(symbol) {
-            fill(0, 255, 70);
+            if (symbol.first) {
+                fill(190, 255, 190);
+            } else {
+                fill(0, 255, 70);
+        }
             text(symbol.value, symbol.x, symbol.y);
             symbol.rain();
             symbol.setToRandomSymbol();
